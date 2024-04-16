d8f99c4357d280cde34015b23acdb7859bbdb1c0
hesor85 <hesor@cs.aau.dk>
2024-04-16 10:51:37 +0200

    Added movies service and result component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MoviesService } from './movies.service';
+import { ResultComponent } from './result/result.component';
+import { CommonModule, NgForOf, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ResultComponent, NgForOf, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'movies';
+  data: Array<any> = [];
+
+  constructor(private moviesService: MoviesService) {}
+
+  getMovies() {
+    this.moviesService.getMovies().subscribe((result: any) => {
+      this.data = result;
+    });
+  }
 }
---
+++ src/app/movies.service.ts
@@ -0,0 +1,14 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MoviesService {
+
+  constructor(private http: HttpClient) { }
+
+  getMovies() {
+    return this.http.get('/api/movies');
+  }
+}
